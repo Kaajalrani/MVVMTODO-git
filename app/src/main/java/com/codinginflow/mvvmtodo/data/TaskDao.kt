27a41a5eda9af6a76fa8969c180a4d6489e8fbd0
Kaajalrani <kajalnegi3479@gmai.com>
2022-01-21 12:11:20 +0530

    Combining flow done

--- app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
@@ -1,14 +1,26 @@
 package com.codinginflow.mvvmtodo.data
 
 import androidx.room.*
+import com.codinginflow.mvvmtodo.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
+
+    fun getTasks(query: String,sortOrder: SortOrder,hideCompleted: Boolean):Flow<List<Task>> =
+    when(sortOrder){
+        SortOrder.BY_DATE->getTasksSortedByDateCreated(query,hideCompleted)
+        SortOrder.BY_NAME->getTasksSortedByName(query,hideCompleted)
+
+    }
+
     //query will return all task in aur task table.
-    @Query("SELECT * FROM task_table WHERE name LIKE '%'||:searchQuery ||'%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String) : Flow<List<Task>>//to get data outOf db
+    @Query("SELECT * FROM task_table WHERE (completed!= :hideCompleted OR completed=0) AND name LIKE '%'||:searchQuery ||'%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted:Boolean) : Flow<List<Task>>//to get data outOf db
+
+    @Query("SELECT * FROM task_table WHERE (completed!= :hideCompleted OR completed=0) AND name LIKE '%'||:searchQuery ||'%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted:Boolean) : Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task : Task)
